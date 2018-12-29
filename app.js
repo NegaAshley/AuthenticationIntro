73b66e8b07b2e117e2a5ed327d9c85b5a9219d81
NegaAshley <ashleymxu@gmail.com>
2018-12-29 08:37:38 +0000

    Handle user signup using Passport

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var express                 = require("express"),
     bodyParser              = require("body-parser"),
     LocalStrategy           = require("passport-local"),
     passportLocalMongoose   = require("passport-local-mongoose"),
-    user                    = require("./models/user");
+    User                    = require("./models/user");
 
 mongoose.connect("mongodb://localhost/auth_demo_app");
 
@@ -18,9 +18,10 @@ app.use(require("express-session")({
 app.set("view engine", "ejs");
 app.use(passport.initialize());
 app.use(passport.session());
+app.use(bodyParser.urlencoded({extended: true}));
 
-passport.serializeUser(user.serializeUser());
-passport.deserializeUser(user.deserializeUser());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 // =======
 // ROUTES
@@ -43,7 +44,20 @@ app.get("/register", function(req, res){
 
 //Handle user signup
 app.post("/register", function(req, res){
-    res.send("Register post route");
+    //Hashes password
+    User.register(new User({username: req.body.username}), req.body.password, function(err, user){
+        if(err){
+            console.log("Error creating user!");
+            console.log(err);
+            return res.render("register");
+        }else{
+            //Logs user in, takes care of session, runs serialize user method
+            //using local strategy
+            passport.authenticate("local")(req, res, function(){
+               res.redirect("/secret"); 
+            });
+        }
+    });
 });
 
 app.listen(process.env.PORT, process.env.IP);
