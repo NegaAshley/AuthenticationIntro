adcdea5bb880ff9e30295da2e5a23c3a5b211381
NegaAshley <ashleymxu@gmail.com>
2018-12-29 05:44:53 +0000

    Add user model and require it in app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var app = express();
 var mongoose = require("mongoose");
+var user = require("./models/user");
 
 mongoose.connect("mongodb://localhost/auth_demo_app");
 app.set("view engine", "ejs");
